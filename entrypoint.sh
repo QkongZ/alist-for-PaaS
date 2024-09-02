c5d1be6cb2f40ea9d948548a9d9ec3af74043ece
iZen <96138710+QkongZ@users.noreply.github.com>
2024-09-03 01:12:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ fi
 if [ "$1" = "version" ]; then
   ./alist version
 else
-  su-exec ${PUID}:${PGID} ./alist server --no-prefix
+  exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
 fi
 
 ########################################################################################
