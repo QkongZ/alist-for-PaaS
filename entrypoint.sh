ab4b26da2051d4429362dd0bbcb58c3069a3c872
iZen <96138710+QkongZ@users.noreply.github.com>
2024-09-03 02:44:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,27 +15,22 @@ SUPERVISORD_CONFIG_PATH="/etc/supervisord.conf"
 # 设置权限和掩码
 chown -R ${PUID}:${PGID} /opt/alist/
 umask ${UMASK}
-
+nginx
 ########################################################################################
 # 生成 Supervisor 配置文件
 cat > ${SUPERVISORD_CONFIG_PATH} << EOF
 [supervisord]
 nodaemon=true
+logfile=/var/log/supervisord.log
 
 [program:nginx]
 command=/usr/sbin/nginx -g 'daemon off;'
-autostart=true
 autorestart=true
-stdout_logfile=/dev/stdout
-stderr_logfile=/dev/stderr
 
 [program:alist]
 command=/opt/alist/alist server --no-prefix
-autostart=true
 autorestart=true
 user=$(whoami)
-stdout_logfile=/dev/stdout
-stderr_logfile=/dev/stderr
 EOF
 
 # 如果设置了 Nezha 相关变量，添加 nezha-agent 到 Supervisor 配置
@@ -45,8 +40,6 @@ if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
 command=nezha-agent -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_ARGS}
 autostart=true
 autorestart=true
-stdout_logfile=/dev/stdout
-stderr_logfile=/dev/stderr
 EOF
 else
   echo "NEZHA_SERVER, NEZHA_PORT, 或 NEZHA_KEY 未设置，跳过 nezha-agent 配置。"
@@ -71,14 +64,17 @@ fi
 sed -i "s/^ID=.*/ID=${PLATFORM}/; s/^VERSION_ID=.*/VERSION_ID=${VERSION}/" /etc/os-release
 
 ########################################################################################
-# 启动 Supervisor
-echo "启动 supervisord 以管理服务..."
-supervisord -c ${SUPERVISORD_CONFIG_PATH}
-
-########################################################################################
 # 如果参数是 version，则显示 alist 版本
 if [ "$1" = "version" ]; then
   ./alist version
 else
   exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
 fi
+
+########################################################################################
+# 启动 Supervisor
+echo "启动 supervisord 以管理服务..."
+supervisord -c ${SUPERVISORD_CONFIG_PATH}
+
+
+
